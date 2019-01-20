5b8f898ce8441421178e856e3aa6532f8ae061f9
trav15 <travistokuyama@gmail.com>
2019-01-20 17:22:21 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
+
+function showRepositories() {
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(r => '<li>' + r.name + '</li>')
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
+}
